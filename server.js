950c37d4da2fc24b0d374f91d9071b6f33c4a847
Divya Jayaraman <divyajayaram1210@gmail.com>
2025-04-07 14:13:11 +0530

    Resolved merge conflict in server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -18,24 +17,3 @@ const db = require('./config/db');
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-=======
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// ✅ Only declare this ONCE
-const productRoutes = require('./routes/productRoutes');
-app.use('/api/products', productRoutes);
-
-// DB connection + Start server
-const db = require('./config/db');
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
->>>>>>> 29aaf62030cbedbc5305b04a15b3fd4128eca610
